397402281f9ae886430c344b1c1270683d147643
Shivani <96899982+shivani-tripurari@users.noreply.github.com>
2024-09-22 10:30:42 +0530

    Update Grid.jsx

--- my-app/app/components/Grid.jsx
+++ my-app/app/components/Grid.jsx
@@ -9,7 +9,7 @@ const Grid = () => {
   return (
     <>
 
-        <div className='relative pt-20 h-[220vh] lg:[100vh] w-[100%] bg-hero-pattern bg-background object-contain'>
+        {/* <div className='relative pt-20 h-[220vh] lg:[100vh] w-[100%] bg-hero-pattern bg-background object-contain'>
             <h1 className='text-3xl md:3xl lg:text-6xl py-7 flex justify-center items-center text-center lg:text-right lg:mr-20 lg:justify-end' >BUILDING THE FUTURE 
                 <br/> OF ECO LIVING</h1>
             <div className='block lg:flex lg:justify-center lg:items:center'>
@@ -22,7 +22,7 @@ const Grid = () => {
                 </div>
                 <div className='block px-7 lg:flex lg:flex-col lg:justify-evenly lg:items:center'>
                     <div className='bg-white h-[20rem] mb-3 w-full lg:w-[20rem]' ><Image src={img4} className='h-full w-full object-cover'/></div>
-                    <div className='bg-white h-[20rem] mb-3 w-full lg:w-[20rem]' ><Image src={img5} className='h-full w-full object-cover'/></div>
+                    <div className='bg-white h-[20rem] mb-3 w-full lg:w-[20rem]' ><Image src={img5} className='h-full w-full object-cover'/></div> */}
 
         <div className='relative pt-20 h-[170vh] md:h-[100vh] lg:h-[140vh] w-[100%] bg-hero-pattern bg-background object-contain'>
             <h1 className='text-3xl md:3xl lg:text-6xl py-7 flex justify-center items-center text-center lg:text-right lg:mr-20 lg:justify-end' >BUILDING THE FUTURE 
