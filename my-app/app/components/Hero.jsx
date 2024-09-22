e4d24fee6ded4c63c7eb18b91c145556756ae989
Shivani <96899982+shivani-tripurari@users.noreply.github.com>
2024-09-22 16:57:44 +0530

    Update Hero.jsx

--- my-app/app/components/Hero.jsx
+++ my-app/app/components/Hero.jsx
@@ -6,13 +6,13 @@ import img1 from '../assets/ayatiHero.png'
 const Hero = () => {
   return (
     <>
-    <div className='relative h-[100vh] w-[100%] bg-hero-pattern bg-background object-contain'>
+    <div className='relative h-[100vh] md:h-[50vh] lg:h-[100vh] w-[100%] bg-hero-pattern bg-background object-contain'>
         <Navbar/>
-        <div className='absolute top-36 block mt-0 md:flex md:items-center md:justify-evenly lg:flex lg:items-center lg:justify-evenly '>
+        <div className='absolute top-36 block mt-0 md:top-20 md:flex md:items-center md:justify-evenly lg:top-36 lg:flex lg:items-center lg:justify-evenly '>
             <div className='mr-7'>
 
 
-                <h1 className='text-3xl text-[#FBBC89] ml-36 my-7 lg:text-7xl'>MINDFUL
+                <h1 className='text-3xl text-[#FBBC89] ml-36 my-7 md:text-4xl lg:text-7xl'>MINDFUL
 
     
 
@@ -21,7 +21,7 @@ const Hero = () => {
                 {/* <h3>Ayati Lake View</h3>
                 <p>View project</p> */}
             </div>
-            <div className='h-[33rem] md:w-full md:w-4/5 lg:w-4/5 bg-white'>
+            <div className='h-[33rem] w-full md:h-[24rem] md:w-4/5 lg:w-4/5 lg:h-[33rem] bg-white'>
                 <Image src={img1} className='h-full w-full object-cover bg-no-repeat' /> 
                 {/* Scroll images and chnaging text */}
             </div>
